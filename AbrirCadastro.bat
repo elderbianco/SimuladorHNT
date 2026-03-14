f743076f9a5f6cc3ccdfa77197837c4c715318ac
elderbianco <elderbianco@yahoo.com.br>
2026-03-14 09:23:50 -0300

    Auto-sync: 14/03/2026  9:23:50,27

---
+++ AbrirCadastro.bat
@@ -0,0 +1,5 @@
+@echo off
+title Abrir Cadastro - Hanuthai
+echo Abrindo a página de Cadastro no navegador...
+start "" "%~dp0indexCadastro.html"
+exit
---
+++ AbrirCarrinho.bat
@@ -0,0 +1,5 @@
+@echo off
+title Abrir Carrinho - Hanuthai
+echo Abrindo o Carrinho no navegador...
+start "" "%~dp0IndexPedidoSimulador.html"
+exit
